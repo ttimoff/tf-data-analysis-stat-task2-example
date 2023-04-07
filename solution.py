2aa38339ee572557efcae62d313a5001386f7dc4
ttimoff <47722752+ttimoff@users.noreply.github.com>
2023-04-07 18:00:44 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -10,5 +10,5 @@ def solution(p: float, x: np.array) -> tuple:
     alpha = 1 - p
     n = len(x)
     loc = x.mean()
-    return (loc - gamma(1, n).ppf(1 - alpha / 2) / n - 1 / 2) * 2 / 26 ** 2, \
-           (loc - gamma(1, n).ppf(alpha / 2) / n - 1 / 2) * 2 / 26 ** 2
+    return (loc + gamma.ppf(alpha / 2, a=n) / n - 1 / 2) * 2 / 26 ** 2, \
+           (loc + gamma.ppf(1 - alpha / 2, a=n) / n - 1 / 2) * 2 / 26 ** 2
